1ebc9f814a4eb26470b21a593496bfa1d7882f0a
Sanghean Lee <francesco419@naver.com>
2022-04-20 19:07:53 +0900

    10th-text

--- src/objects/Project.js
+++ src/objects/Project.js
@@ -2,36 +2,38 @@ import styles from "./Project.module.css";
 import {Link} from "react-router-dom";
 import Aos from "aos";
 import "aos/dist/aos.css";
+import mv from "./picture/movie.PNG";
+import pf from "./picture/portfolio.PNG";
 
 function Project({id,num}){
     const prj = [
         {
             id:1,
             imgs:"https://picsum.photos/1000/500",
-            title:"유투브 클론 프로젝트1",
-            skill:"React, Java Script",
-            text:"리엑트를 사용하여 유튜브 전반적인 기능을동일하게 구현한 프로젝트입니다\n리엑트를 사용한 페이지 전환 및 라이브러리에 대해 더욱 발전시킬 수 있었던 계기가 되었습니다."
+            title:"인스타웹클론",
+            skill:"HTML, CSS",
+            text:"인스타그램의 프로필 페이지를 클론한 프로젝트입니다."
         },
         {
             id:2,
-            imgs:"https://picsum.photos/1000/500",
-            title:"유투브 클론 프로젝트2",
-            skill:"React, Java Script",
-            text:"리엑트를 사용하여 유튜브 전반적인 기능을동일하게 구현한 프로젝트입니다\n리엑트를 사용한 페이지 전환 및 라이브러리에 대해 더욱 발전시킬 수 있었던 계기가 되었습니다."
+            imgs:"https://user-images.githubusercontent.com/84317858/158579317-6d81584f-6cce-435f-b6d0-e3dc2c8065a3.PNG",
+            title:"Greeting WebPage",
+            skill:"HTML, CSS, JavaScript",
+            text:"시간 및 날씨 정보와 할 일 등의 정보를 기록할 수 있는 웹페이지 프로젝트입니다."
         },
         {
             id:3,
-            imgs:"https://picsum.photos/1000/500",
-            title:"유투브 클론 프로젝트3",
-            skill:"React, Java Script",
-            text:"리엑트를 사용하여 유튜브 전반적인 기능을동일하게 구현한 프로젝트입니다\n리엑트를 사용한 페이지 전환 및 라이브러리에 대해 더욱 발전시킬 수 있었던 계기가 되었습니다."
+            imgs:mv,
+            title:"Movie Theater",
+            skill:"HTML, CSS, JavaScript, REACT",
+            text:"영화정보"
         },
         {
             id:4,
-            imgs:"https://picsum.photos/1000/500",
-            title:"유투브 클론 프로젝트4",
-            skill:"React, Java Script",
-            text:"리엑트를 사용하여 유튜브 전반적인 기능을동일하게 구현한 프로젝트입니다\n리엑트를 사용한 페이지 전환 및 라이브러리에 대해 더욱 발전시킬 수 있었던 계기가 되었습니다."
+            imgs:pf,
+            title:"포트폴리오",
+            skill:"HTML, CSS, JS, REACT",
+            text:"개인 포트폴리오를 제작한 프로젝트입니다."
         }
     ]
 
