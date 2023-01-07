b8c1c3419cce0e75e62038908dda006684cc783d
Lee Sang Hyeon <francesco419@naver.com>
2023-01-07 23:03:21 +0900

    11

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -2,37 +2,43 @@ import { useEffect, useState } from "react";
 import styles from "./Loading.module.css";
 import useInterval from "use-interval";
 
-function Loading(){
-    const [a,setA]=useState(0);
-    const [b,setB]=useState(0);
-    useEffect(()=>{
-        setB(b+1);
-    },[a])
-    useInterval(()=>{
-        if(a===99){
-            return;
-        }else{
-            aaa();
-        }
-    },30);
-    const aaa=()=>{
-        setA(a+1);
+function Loading() {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  useEffect(() => {
+    setB(b + 1);
+  }, [a]);
+
+  useInterval(() => {
+    if (a === 99) {
+      return;
+    } else {
+      aaa();
     }
-    return(
-        <div style={{animationDelay:'3.5s'}} className={styles.page}>
-            <div style={{marginBottom:'20px'}}>{b}%</div>
-            <div className={styles.box}>
-                <div className={styles.load}>
+  }, 30);
+
+  const aaa = () => {
+    setA(a + 1);
+  };
+
+  return (
+    <div style={{ animationDelay: "3.5s" }} className={styles.page}>
+      <div style={{ marginBottom: "20px" }}>{b}%</div>
+      <div className={styles.box}>
+        <div className={styles.load}>
+          <div
+            style={{ animationDelay: "0.5s" }}
+            className={styles.lineB}
+          ></div>
+          {/*    <div style={{animationDelay:'0.5s'}} className={styles.lineB}></div>
                     <div style={{animationDelay:'0.5s'}} className={styles.lineF}></div>
                     <div style={{animationDelay:'0.5s'}} className={styles.lineB}></div>
-                    <div style={{animationDelay:'0.5s'}} className={styles.lineF}></div>
-                    <div style={{animationDelay:'0.5s'}} className={styles.lineB}></div>
-                    <div style={{animationDelay:'0.5s'}} className={styles.lineF}></div>
-                </div>
-            </div>
+                    <div style={{animationDelay:'0.5s'}} className={styles.lineF}></div> */}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Loading;
-
