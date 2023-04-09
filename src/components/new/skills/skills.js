1afe995216f12e9cd43b27ad8892ea5ea1eb7a0d
Lee Sang Hyeon <francesco419@naver.com>
2023-04-09 23:53:44 +0900

    230409

--- src/components/new/skills/skills.js
+++ src/components/new/skills/skills.js
@@ -10,57 +10,57 @@ import { ReactComponent as TYPE } from '../../../assets/image/skills/type.svg';
 const SKILLINFORMATION = [
   {
     no: 0,
-    svg: [<CSS />, <HTML />],
-    name: 'CSS HTML',
-    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ornare
-        volutpat fringilla. Curabitur vitae ultricies lectus, quis facilisis
-        mauris. Nunc dictum volutpat nisl, eu fermentum tellus. Duis sem sapien,
-        lobortis.`
+    svg: [<HTML />, <CSS />],
+    name: 'HTML CSS',
+    text: [
+      '기본이라고 할 수 있는 HTML/CSS를 자유자재로 구현할 수 있습니다.',
+      '프로덕트에 요구되는 적절한 애니메이션을 Keyframe / transition 등을 통해 구현할 수 있습니다.',
+      'SCSS 전처리기 혹은 styled-component와 같은 css-in-js를 사용할 수 있습니다.'
+    ]
   },
   {
     no: 1,
     svg: [<JS />],
     name: 'JAVASCRIPT',
-    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ornare
-        volutpat fringilla. Curabitur vitae ultricies lectus, quis facilisis
-        mauris. Nunc dictum volutpat nisl, eu fermentum tellus. Duis sem sapien,
-        lobortis.`
+    text: [
+      'ES6 이후의 JS를 사용하는것에 능숙하며,',
+      'JS를 사용하여 CSS보다 복잡한 애니메이션을 구현 할 수 있습니다.',
+      'DOM 조작 및 이벤트 할당을 통해 인터렉티브한 코드를 작성 할 수 있습니다.'
+    ]
   },
   {
     no: 2,
     svg: [<REACT />],
     name: 'REACT',
-    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ornare
-        volutpat fringilla. Curabitur vitae ultricies lectus, quis facilisis
-        mauris. Nunc dictum volutpat nisl, eu fermentum tellus. Duis sem sapien,
-        lobortis.`
+    text: [
+      `함수형 컴포넌트 형태의 작성을 선호합니다.`,
+      '라이프 사이클을 이해하여 최적의 랜더링을 통한 최고의 프로덕트를 만들고자 합니다.',
+      'React Hook을 적절히 사용하여 랜더링을 제어하고 필요시 새로운 Hook을 제작 할 수 있습니다..'
+    ]
   },
   {
     no: 3,
     svg: [<REDUX />],
     name: 'REDUX',
-    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ornare
-        volutpat fringilla. Curabitur vitae ultricies lectus, quis facilisis
-        mauris. Nunc dictum volutpat nisl, eu fermentum tellus. Duis sem sapien,
-        lobortis.`
+    text: [
+      'Redux를 사용하여 props 복잡도를 해결하고, 스토어를 통한 프로젝트의 효율적인 상태관리를 할 수 있습니다.',
+      'Redux toolkit 사용을 선호합니다. 보다 쉽고 간편하게 상태관리를 할 수 있다고 생각합니다.',
+      'Redux-persist를 사용하여 storage에 데이터를 저장하고 이를 효율적으로 사용 및 관리하고자 합니다.'
+    ]
   },
   {
     no: 4,
     svg: [<SCSS />],
     name: 'SCSS',
-    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ornare
-        volutpat fringilla. Curabitur vitae ultricies lectus, quis facilisis
-        mauris. Nunc dictum volutpat nisl, eu fermentum tellus. Duis sem sapien,
-        lobortis.`
+    text: [
+      '변수/ 중첩/ 함수 등의 문법을 사용하여 재사용성을 높이는 스타일시트 작성을 할 수 있습니다.'
+    ]
   },
   {
     no: 5,
     svg: [<TYPE />],
     name: 'TYPESCRIPT',
-    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ornare
-        volutpat fringilla. Curabitur vitae ultricies lectus, quis facilisis
-        mauris. Nunc dictum volutpat nisl, eu fermentum tellus. Duis sem sapien,
-        lobortis.`
+    text: [`정적코드작성이 가능합니다.`]
   }
 ];
 
@@ -73,7 +73,11 @@ export default function Skills({ num = 0 }) {
         })}
       </div>
       <p className={styles['skills__name']}>{SKILLINFORMATION[num].name}</p>
-      <p className={styles['skills__detail']}>{SKILLINFORMATION[num].text}</p>
+      <ul>
+        {SKILLINFORMATION[num].text.map((data) => {
+          return <li className={styles['skills__detail']}>{data}</li>;
+        })}
+      </ul>
     </div>
   );
 }
