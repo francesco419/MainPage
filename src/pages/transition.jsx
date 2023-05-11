b629470be2adfcc9a3bb7abec63da359a514e7d5
Lee Sang Hyeon <francesco419@naver.com>
2023-05-12 06:25:53 +0900

    230512 fix 3

--- src/pages/transition.jsx
+++ src/pages/transition.jsx
@@ -18,7 +18,6 @@ const Transition = () => {
     <TransitionGroup className='transition-group'>
       <CSSTransition key={location.pathname} classNames='fade' timeout={500}>
         <Routes location={location}>
-          <Route path={`${process.env.PUBLIC_URL}`} element={<FirstPage />} />
           <Route path='/intro' element={<Introduction />} />
           <Route path='/sec' element={<SecondPage />} />
           <Route path='/asec' element={<AfterSec />} />
@@ -27,6 +26,7 @@ const Transition = () => {
           <Route path='/ProjectNav/:id' element={<ProjectNav />} />
           <Route path='/List' element={<List />} />
           <Route path='/hidden' element={<Hidden />} />
+          <Route path={`${process.env.PUBLIC_URL}`} element={<FirstPage />} />
         </Routes>
       </CSSTransition>
     </TransitionGroup>
